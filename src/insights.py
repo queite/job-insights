fb4c589ff2ea9683fa251277a989dd96e2c4f033
Queite <queitesc@gmail.com>
2022-10-20 16:14:00 -0300

    feat: :sparkles: add get_max_salary and get_min_salary functions

--- src/insights.py
+++ src/insights.py
@@ -48,39 +48,19 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs = read(path)
+    max_salaries = {
+        int(job["max_salary"]) for job in jobs if job["max_salary"].isdigit()
+    }
+    return max(max_salaries)
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    jobs = read(path)
+    min_salaries = {
+        int(job["min_salary"]) for job in jobs if job["min_salary"].isdigit()
+    }
+    return min(min_salaries)
 
 
 def matches_salary_range(job, salary):
